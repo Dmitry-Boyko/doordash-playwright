91f3389e120fba7bb113a9b7199e980623a357c9
Dmitry <dmt.b@yahoo.com>
2025-06-17 15:00:34 -0400

    url replaced

--- e2e/landing-page.spec.ts
+++ e2e/landing-page.spec.ts
@@ -1,17 +1,13 @@
 import{test, expect} from '@playwright/test'
-import { clickButton, clickButtonByTestId, typeIntoFieldByTestId } from '../util/helper'
+import { clickByText, hoverByText } from '../util/helper'
 
 test.describe('landing page', () => {
   test('login page test', async ({ page }) => {
-    await page.goto('https://www.doordash.com/')
-    await page.isVisible("text='Verifying you are human. This may take a few seconds.'")
-    await expect(page.getByText('$0 DELIVERY FEE ON FIRST ORDER')).toBeVisible({ timeout: 10000 });
+    await page.goto('https://www.foxtailcoffee.com/')
+    await page.isVisible("text='WHERE COFFEE MEETS COMMUNITY'")
 
-    await clickButtonByTestId(page, 'identityLastLoginButton')
-    await clickButtonByTestId(page, 'fieldWrapper-:rf:')
-
-    await typeIntoFieldByTestId(page, 'fieldWrapper-:rf:', 'dmt.b@yahoo.com')
-
+    await hoverByText(page, 'LOCATIONS')
+    await clickByText(page, 'ALL LOCATIONS')
 
   })
 })
--- util/helper.ts
+++ util/helper.ts
@@ -1,13 +1,22 @@
 import { Page } from '@playwright/test';
 
 
-const clickButton = async (buttonName: string) => {
+const clickButton = async (page: Page, buttonName: string) => {
   await page.getByRole('button', { name: buttonName }).click()
 }
 
 const clickButtonByTestId = async (page: Page, testId: string) => {
     await page.getByTestId(testId).click();
 };
+
+const hoverByText = async (page: Page, buttonText: string) => {
+  await page.waitForTimeout(5000)
+  await page.getByText(buttonText).nth(0).hover();
+}
+
+const clickByText = async (page: Page, buttonText: string) => {
+  await page.getByText(buttonText).nth(0).click();
+}
 
 const typeIntoFieldByTestId = async (page: Page, testId: string, text: string) => {
     await page.getByTestId(testId).fill(text);
@@ -17,5 +26,7 @@ const typeIntoFieldByTestId = async (page: Page, testId: string, text: string) =
 export { 
   clickButton, 
   clickButtonByTestId,
+  hoverByText,
+  clickByText,
   typeIntoFieldByTestId
  }
